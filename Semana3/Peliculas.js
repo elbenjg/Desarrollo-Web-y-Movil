06164b614bbaa8278c78b4027b5887cb8b8a0575
elbenjg <b.arayagonlez@uandresbello.edu>
2026-08-22 15:50:37 -0400

    Co-authored-by: Jaime-Ariza <Jaime-Ariza@users.noreply.github.com>

---
+++ Semana3/Peliculas.js
@@ -0,0 +1,12 @@
+const Peliculas = {
+    "status": 200,
+    "message": "Productos obtenidos correctamente",
+    "data": [
+        {"id": 1,"nombre": "Spider-Man", "Año": 2002, "imagen": "https://es.web.img2.acsta.net/medias/nmedia/18/90/04/41/20078157.jpg"},
+        {"id": 2,"nombre": "Spider-Man 2", "Año": 2004, "imagen": "https://m.media-amazon.com/images/M/MV5BNGQ0YTQyYTgtNWI2YS00NTE2LWJmNDItNTFlMTUwNmFlZTM0XkEyXkFqcGc@._V1_.jpg"},
+        {"id": 3,"nombre": "Spider-Man 3", "Año": 2007, "imagen": "https://m.media-amazon.com/images/M/MV5BODE2NzNhMDctYjUzMC00Y2M5LWI2Y2EtODJkZTFjN2Y5ODlmXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg"},
+        {"id": 4,"nombre": "The Shining", "Año": 1980, "imagen": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsfuzIEX8nD_OMJ6AwZ-awygwtojO_W3YtLTCh4xmk_xIlXy9CXR9qN_s3&s=10"},
+        {"id": 5,"nombre": "Coraline", "Año": 2009, "imagen": "https://cdn2.steamgriddb.com/thumb/850660ba5cb014184e5cc0e6b3d4c6bb.png"},
+        ]
+};
+
